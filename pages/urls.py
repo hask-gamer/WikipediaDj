138fc1e8886767af3c86b2c2fa5eb536591cf6d6
hask-gamer <iegamberdiev873@gmail.com>
2024-01-30 16:36:05 +0500

    project finished

---
+++ pages/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from .views import *
+
+
+urlpatterns = [
+    path('', home_page, name='home'),
+    path('login/', login_page, name='login'),
+    path('register/', register_page, name='register'),
+    path('logout/', user_logout, name='logout'),
+    path('create_page/', create_page, name='create_page'),
+    path('all_pages/', all_pages, name='all_pages'),
+    path('detail_page/<int:page_id>/', detail_page, name='detail_page'),
+    path('del_page/<int:page_id>/', del_page, name='del_page'),
+    path('edit_page/<int:page_id>/', edit_page, name='edit_page'),
+    path('random_page/', random_page, name='random_page'),
+]
